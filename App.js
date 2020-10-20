57f7908b6c07625c92a21eff36583d8ff2045e4a
Renato Britto Araujo <renatobritto@protonmail.com>
2020-10-20 12:13:07 -0300

    Complete pages view and create page management

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text
+  Text,
+  AsyncStorage
 } from 'react-native';
 
 import DataCollection from './views/DataCollection'
 import Dashboard from './views/Dashboard'
 import Loading from './views/Loading'
 
-const App = () => {
-  return (
-    <>
-      <DataCollection/>
-    </>
-  );
+class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      page: null
+    }
+    this.findLoadedUser()
+  }
+
+  async findLoadedUser () {
+    try {
+      const loadedUser = await AsyncStorage.getItem('loadedUser')
+      if (loadedUser == 'true') {
+        this.setState({page: 'Dashboard'})
+      } else {
+        this.setState({page: 'DataCollection'})
+      }
+    } catch (error) {
+      this.setState({page: 'DataCollection'})
+    }
+  }
+
+  changePage (page) {
+    this.setState({page: page})
+  }
+
+  render () {
+    if (this.state.page === 'DataCollection') {
+      return (<DataCollection changePage={(e) => this.changePage(e)}/>)
+    } else if (this.state.page === 'Dashboard') {
+      return (<Dashboard/>)
+    } else {
+      return (<Loading/>)
+    }
+  }
 };
 
 const styles = StyleSheet.create({
--- views/DataCollection.js
+++ views/DataCollection.js
@@ -1,57 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
   Text,
   TextInput
-} from 'react-native';
+} from 'react-native'
+import { Picker } from '@react-native-community/picker';
+import { AsyncStorage } from 'react-native';
 
 import LogoWithText from '../components/LogoWithText'
 import CustomButton from '../components/CustomButton'
 
-const DataCollection = () => {
-  return (
-    <>
+class DataCollection extends Component {
+  
+  constructor (props) {
+    super(props)
+    this.state = {
+      sex:          null,
+      vehicle_type: null,
+      vehicle_age:  null,
+      age:          null,
+    }
+  }
+
+  async storeData () {
+    await AsyncStorage.setItem('loadedUser',    String(true))
+    await AsyncStorage.setItem('sex',           this.state.sex)
+    await AsyncStorage.setItem('vehicle_type',  this.state.vehicle_type)
+    await AsyncStorage.setItem('vehicle_age',   this.state.vehicle_age)
+    await AsyncStorage.setItem('age',           this.state.age)
+
+    this.props.changePage('Dashboard')
+  }
+
+  render () {
+    return (
+      <>
       <View style={styles.container}>
-            <LogoWithText/>
-            <View style={styles.textbox}>
-                <Text style={styles.text}> Fill some data about yourself so we can better predict your risks. </Text>
-                <Text style={styles.text}> It's optional and annonymous! </Text>
-            </View>
-            <View style={styles.form}>
-              <View style={styles.formdata}>
-              <TextInput style={styles.formtextinput} placeholder="Age"></TextInput>
-              </View>
-              <View style={styles.formdata}>
-                <Text style={styles.formtext}>Sex</Text>
-                <Text style={styles.formdropdown}>batata</Text>
-              </View>
-              <View style={styles.formdata}>
-                <TextInput style={styles.formtextinput} placeholder="Vehicle Age"></TextInput>
-              </View>
-              <View style={styles.formdata}>
-                <Text style={styles.formtext}>Vehicle Type</Text>
-                <Text style={styles.formdropdown}>batata</Text>
-              </View>
-            </View>
-            <View style={styles.buttons}>
-              <CustomButton style={styles.button} title="Send Data"></CustomButton>
-              <View style={styles.button}></View>
-              <CustomButton style={styles.button} title="Skip"></CustomButton>
-            </View>
+        <LogoWithText/>
+        <View style={styles.textbox}>
+            <Text style={styles.text}> 
+              Fill some data about yourself so we can better predict your risks.
+            </Text>
+            <Text style={styles.text}> 
+              It's optional and annonymous! 
+            </Text>
+        </View>
+        <View style={styles.form}>
+          <View style={styles.formdata}>
+          <TextInput 
+            style={styles.formtextinput} 
+            keyboardType='phone-pad' 
+            onChangeText={text => this.setState({sex: text})} 
+            placeholder="Age"/>
+          </View>
+          <View style={styles.formpickercontainer}>
+            <Text style={styles.formpickertext}>Sex</Text>
+            <Picker
+              selectedValue={this.state.sex}
+              style={styles.formpicker}
+              itemStyle={styles.formpickeritemstyle}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({sex: itemValue})
+              }>
+              <Picker.Item label="Male" value="male" />
+              <Picker.Item label="Female" value="female" />
+              <Picker.Item label="Other" value={null} />
+            </Picker>
+          </View>
+          <View style={styles.formdata}>
+            <TextInput 
+              style={styles.formtextinput} 
+              keyboardType='phone-pad' 
+              onChangeText={text => this.setState({vehicle_age: text})} 
+              placeholder="Vehicle Age"/>
+          </View>
+          <View style={styles.formpickercontainer}>
+            <Text style={styles.formpickertext}>Vehicle type</Text>
+            <Picker
+              selectedValue={this.state.vehicle_type}
+              style={styles.formpicker}
+              itemStyle={styles.formpickeritemstyle}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({vehicle_type: itemValue})
+              }>
+              <Picker.Item label="Car" value="car" />
+              <Picker.Item label="Motorcycle" value="motorcycle" />
+              <Picker.Item label="Other" value={null} />
+            </Picker>
+          </View>
+        </View>
+        <View style={styles.buttons}>
+          <CustomButton 
+            style={styles.button} 
+            title="Send Data" 
+            onPress={this.storeData.bind(this)}/>
+          <View style={styles.button}></View>
+          <CustomButton 
+            style={styles.button} 
+            title="Skip" 
+            onPress={this.storeData.bind(this)}/>
+        </View>
       </View>
     </>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: 'center',
-        flexDirection: "column",
-        backgroundColor: '#E3EFF5',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+    flexDirection: "column",
+    backgroundColor: '#E3EFF5',
+  },
     textbox: {
       marginTop: 10,
       alignItems: 'center',
@@ -70,26 +133,49 @@ const styles = StyleSheet.create({
     },
     formdata: {
       flexDirection: 'row',
-      marginBottom: 10
+      
+    },
+    formpickertext: {
+      fontFamily: 'Raleway-Thin',
+      textAlign: 'center',
+    },
+    formpicker: {
+      height: 20,
+      marginLeft: 10,
+      marginBottom: 5,
+      fontFamily: 'Raleway-Thin',
+      textAlign: 'center',
+    },
+    formpickeritemstyle: {
+      fontFamily: 'Raleway-Thin',
+      textAlign: 'center',
+      fontSize: 5
+    },
+    formpickercontainer: {
+      flexDirection: 'column',
+      marginBottom: 10,
+      textAlign: 'center',
+      marginTop: 10,
+      borderColor: '#fff',
+      borderWidth: 2,
+      borderRadius: 10,
+      padding: 5,
+      backgroundColor: '#f7f7f7'
     },
     formtext: {
       flex: 1,
-      fontFamily: 'Raleway-Light',
-    },
-    formdropdown: {
-      flex: 1,
-      textAlign: 'right',
+      fontFamily: 'Raleway-Thin',
     },
     formtextinput: {
+      backgroundColor: '#f7f7f7',
       flex: 1,
-      height: 35,
+      height: 40,
       textAlign: 'center',
       borderWidth: 2,
       borderColor: '#fff',
       padding: 0,
       paddingHorizontal: 7,
       borderRadius: 10,
-
     },
     buttons: {
       flexDirection: 'row',
@@ -101,4 +187,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default DataCollection;
+export default DataCollection
